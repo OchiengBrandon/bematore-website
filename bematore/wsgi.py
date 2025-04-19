977dc2b885b3a758ce4487e3636931ea15c56aa5
brandondeveloper <brandonochieng72@gmail.com>
2025-04-19 19:59:09 +0300

    Added Config for env

--- bematore/wsgi.py
+++ bematore/wsgi.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bematore.settings.development')  
+load_dotenv()
+# Set the default settings module for the 'DJANGO_SETTINGS_MODULE' environment variable.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.environ.get('DJANGO_SETTINGS_MODULE'))  
 
 application = get_wsgi_application()
